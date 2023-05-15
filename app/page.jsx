f774c7f2d3725fb7e816ddbddf481437e6cb0360
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-05-15 05:47:26 -0400

    adds components

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Feed from "@components/Feed";
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -10,7 +11,7 @@ const Home = () => {
         Promptophia is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
-      {/* Feed */}
+      <Feed />
     </section>
   );
 };
