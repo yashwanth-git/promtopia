621d2a42ca71c05993d123edeaaf1129524ef829
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-05-17 03:19:02 -0400

    adds Navbar and Form component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import "@styles/globals.css";
 
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
 export const metadata = {
   title: "Promtopia",
   description: "Discover & Share AI prompts",
@@ -12,7 +15,10 @@ const RootLayout = ({ children }) => {
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
   );
