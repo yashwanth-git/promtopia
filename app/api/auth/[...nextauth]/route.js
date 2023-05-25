59cf1e88d70bb8a3c28b5551f4d7ab76b1cc0ed0
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-05-25 18:02:13 -0400

    fixes unrecognized session

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -13,7 +13,8 @@ const handler = nextAuth({
   callbacks: {
     async session({ session }) {
       const sessionUser = await User.findOne({ email: session.user.email });
-      session.user.id = session._id.toString();
+      session.user.id = sessionUser._id.toString();
+      return session
     },
     async signIn({ profile }) {
       try {
