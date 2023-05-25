0a0b13800a3cee96c462cf2260592e9728769bb7
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-05-25 19:22:47 -0400

    adds prompt model

---
+++ models/prompt.js
@@ -0,0 +1,20 @@
+import { Schemam, model, models } from "mongoose";
+
+const PromptSchema = new Schemam({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is required."],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is required."],
+  },
+});
+
+const Prompt = models.Prompt || model("Prompt", PromptSchema);
+
+export default Prompt;
